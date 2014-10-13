bb8351516bfab207b4268bb1ef6806a12ea4e724
Joanne Caroll <joannecaroll@UNIT001.(none)>
2014-10-13 14:53:37 +0800

    FRagments linked together.

--- CriminalIntent/src/com/example/criminalintent/CrimeActivity.java
+++ CriminalIntent/src/com/example/criminalintent/CrimeActivity.java
@@ -1,25 +1,14 @@
 package com.example.criminalintent;
 
-import android.os.Bundle;
+import java.util.UUID;
+
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
 
 public class CrimeActivity extends SingleFragmentActivity {
-//	@Override
-//		public void onCreate(Bundle savedInstanceState){
-//			super.onCreate(savedInstanceState);
-//			setContentView(R.layout.activity_fragment);
-//			FragmentManager fm = getSupportFragmentManager();
-//			Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
-//			
-//			if(fragment == null){
-//				fragment = new CrimeFragment();
-//				fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
-//			}
-//		}
+
 	@Override
 	protected Fragment createFragment(){
-		return new CrimeFragment();
+		UUID crimeId = (UUID)getIntent().getSerializableExtra(CrimeFragment.EXTRA_CRIME_ID);
+		return CrimeFragment.newInstance(crimeId);
 	}
 }
