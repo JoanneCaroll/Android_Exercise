14c34971468499f48d1e45ce7d1db4993ebece18
Joanne Caroll <joannecaroll@UNIT001.(none)>
2014-10-14 09:20:05 +0800

    audio can be played, retained fragment added.

---
+++ HelloMoon/src/com/example/hellomoon/AudioPlayer.java
@@ -0,0 +1,33 @@
+package com.example.hellomoon;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+public class AudioPlayer {
+	MediaPlayer mPlayer;
+	public void stop(){
+		if(mPlayer != null){
+			mPlayer.release();
+			mPlayer=null;
+		}
+	}
+	public void play(Context c){
+		stop();
+		mPlayer = MediaPlayer.create(c, R.raw.one_small_step);
+		
+		mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+			
+			public void onCompletion(MediaPlayer mp) {
+				stop();				
+			}
+		});
+		
+		mPlayer.start();
+	}
+//	@Override
+//	public void onDestroy(){
+//		super.onDestroy();
+//		mPlayer.stop();
+//	}
+
+}
